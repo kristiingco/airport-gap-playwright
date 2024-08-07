bf93d7aca061b38ce9fb1756b09930f617367926
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-08-06 18:42:39 -0700

    Add test for adding an already favorited airport

--- tests/api/favorites.spec.ts
+++ tests/api/favorites.spec.ts
@@ -4,17 +4,18 @@ test.describe("Favorites", () => {
     const BASE_URL = "https://airportgap.com/api/favorites";
     const token = process.env.AUTH_TOKEN as string;
 
-    test("POST Request - Save an airport as favorite", async ({ request }) => {
+    test.skip("POST Request - Save an airport as favorite", async ({
+        request,
+    }) => {
         const response = await request.post(`${BASE_URL}`, {
             data: {
-                airport_id: "HND",
+                airport_id: "SFO",
             },
             headers: {
                 Authorization: `Bearer token=${token}`,
             },
         });
         const responseBody = JSON.parse(await response.text());
-        console.log(responseBody);
 
         expect(response.status()).toBe(201);
 
@@ -22,9 +23,35 @@ test.describe("Favorites", () => {
 
         const responseDataItemAirportAttributes =
             responseBody.data.attributes.airport;
-        expect(responseDataItemAirportAttributes.iata).toBe("HND");
+        expect(responseDataItemAirportAttributes.iata).toBe("SFO");
 
         expect(responseBody.data.id).toBeTruthy();
         expect(responseBody.data.type).toBe("favorite");
+    });
+
+    test("POST Request - Airport already saved as favorite", async ({
+        request,
+    }) => {
+        const response = await request.post(`${BASE_URL}`, {
+            data: {
+                airport_id: "NRT",
+            },
+            headers: {
+                Authorization: `Bearer token=${token}`,
+            },
+        });
+        const responseBody = JSON.parse(await response.text());
+
+        expect(response.status()).toBe(422);
+
+        expect(responseBody.errors).toBeTruthy();
+
+        const responseBodyErrors = responseBody.errors[0];
+
+        expect(responseBodyErrors.status).toBe("422");
+        expect(responseBodyErrors.title).toBe("Unable to process request");
+        expect(responseBodyErrors.detail).toBe(
+            "Airport This airport is already in your favorites"
+        );
     });
 });
